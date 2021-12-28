84f3553d385ab127cbef2db35d41c1c8ef28dff6
Fernando Coppel <fernando_copp@hotmail.com>
2021-12-28 14:03:24 -0700

    a

--- assets/js/index.js
+++ assets/js/index.js
@@ -85,9 +85,9 @@ $("#add_pago").submit(function(event){
     })
 
     console.log(data)
-    
+
     var request = {
-        "url" : `https://sistema-vet-api.herokuapp.com/api/pagos/${data.id}`,
+        "url" : `https://sistema-vet-api.herokuapp.com/api/pagos/`,
         "method" : "POST",
         "data" : data
     }
